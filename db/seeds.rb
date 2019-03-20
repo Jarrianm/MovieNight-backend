c0624d4c6f37512bbe4230967745dda8c614890f
Jarrianm <jarrian.mclean@gmail.com>
2019-03-20 17:40:14 -0400

    clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,8 +31,7 @@ now_playing.results.each do |movie|
     backdrop_path: movie.backdrop_path,
     release_date: movie.release_date,
     category: "now_playing"
-
-	)
+)
 end
 
  top_rated.results.each do |movie|
@@ -43,8 +42,7 @@ end
     backdrop_path: movie.backdrop_path,
     release_date: movie.release_date,
     category: "top_rated"
-
-	)
+)
 end
 
 popular.results.each do |movie|
@@ -55,17 +53,19 @@ popular.results.each do |movie|
     backdrop_path: movie.backdrop_path,
     release_date: movie.release_date,
     category: "popular"
-	)
+)
 end
 
 # Matching.create!(movie_id: Movie.find_by(id: 1).id,
 # user_id: User.find_by(id: 1).id )
-
+# Matching.create!(movie_id: Movie.find_by(id:1).id, user_id: User.find_by(id:1).id)
 
 jarrian = User.create!(name:"Jarrian", username:"JearBear", location:"NYC",
   age:24, bio:"Lf > Chills", password: "jarrian10",profile_img: "http://i686.photobucket.com/albums/vv226/letsrollny/nyssharlemjamsession/IMG_4710_zpsxao9zsbs.jpg",
-	movie_id: Movie.find_by(id: 1).id)
+	 movie_id: Movie.find_by(id:1).id)
+
 ben = User.create!(name:"Ben", username:"Benny", location:"Jersey",
     age:30, bio:"bored", password: "benny10", profile_img: "https://pbs.twimg.com/profile_images/968200490071875585/NPWafTsh_400x400.jpg")
+
 mikey = User.create!(name:"Michael", username:"Beschwa", location:"FarRock",
     age:27, bio:"gains", password: "michael10", profile_img: "https://a2-images.myspacecdn.com/images03/22/092ec8f6df0b462a9cd6b45715cadb19/300x300.jpg")
